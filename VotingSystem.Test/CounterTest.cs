ea1272dacd96f4ecc4c6b351bd3bf3321abf77a1
Go Chama <marvin.wekesa@gmail.com>
2022-05-16 11:47:13 +0300

    added counter tests

---
+++ VotingSystem.Test/CounterTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VotingSystem.Tests
+{
+    public class CounterTests
+    {
+        [Fact]
+        public void HasName()
+        {
+
+            var name = "Counter Name";
+            var counter = new Counter();
+            counter.Name = name;
+
+            Assert.Equal(name, counter.Name);
+        }
+
+        [Fact]
+        public void GetStatistics_IncludesCounterName()
+        {
+            var name = "Counter Name";
+            var counter = new Counter();
+            counter.Name = name;
+
+            var statistics = counter.GetStatistics();
+            Assert.Equal(name, statistics.Name);
+        }
+    }
+
+    public class Counter
+    {
+        public string Name { get; set;  }
+
+        public Counter()
+        {
+        }
+
+        internal Counter GetStatistics()
+        {
+            return this;
+        }
+    }
+}
--- VotingSystem.Test/UnitTest1.cs
+++ VotingSystem.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Moq;
+using System;
 using Xunit;
 
 namespace VotingSystem.Test
@@ -12,15 +14,51 @@ namespace VotingSystem.Test
         }
 
         public int Add(int a, int b) => testOne.Add(a, b);
+        public void Out(string msg) => testOne.Out(msg);
+    }
+
+    public class MathOneTests
+    {
+        [Fact]
+        public void MathOneAddsTwoNumbers()
+        {
+            var testOneMock = new Mock<ITestOne>();
+            testOneMock.Setup(x => x.Add(1,1)).Returns(2);
+            
+            var mathOne = new MathOne(testOneMock.Object);
+            Assert.Equal(2, mathOne.Add(1, 1));
+        }
+
+        [Fact]
+        public void VerifyFuncrionHasBeenCalled()
+        {
+            var testOneMock = new Mock<ITestOne>();
+            var msg = "Hello";
+
+            var mathOne = new MathOne(testOneMock.Object);
+            mathOne.Out(msg);
+
+            testOneMock.Verify(x => x.Out(msg), Times.Once);
+        }
+
+
     }
     public interface ITestOne
     {
         public int Add(int a, int b);
+        public void Out(string msg);
     }
 
     public class TestOne : ITestOne
     {
         public int Add(int a, int b) => a + b;
+
+        public void Out(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+       
+
     }
 
    
